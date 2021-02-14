b1506425f10b3682eb69219921cf0d635392eda2
Sebastian Hartte <sebastian@hartte.de>
2021-02-14 11:53:22 +0100

    Minimal repro

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpacePreserveProblem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var compiledTextBlock = Content as TextBlock;
+
+            var dynamicSelfClosing = (TextBlock) XamlReader.Parse("<TextBlock xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' xml:space='preserve'> <Run/> </TextBlock>");
+            var dynamicCosing = (TextBlock) XamlReader.Parse("<TextBlock xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' xml:space='preserve'> <Run></Run> </TextBlock>");
+
+            MessageBox.Show(
+                $"Compiled: {compiledTextBlock.Inlines.Count}\n" +
+                $"Dynamic Self-Closing: {dynamicSelfClosing.Inlines.Count}\n" +
+                $"Dynamic Normally Closing:{dynamicCosing.Inlines.Count}");
+            Close();
+        }
+    }
+}
